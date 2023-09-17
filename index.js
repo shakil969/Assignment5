ddec6c11b72580039914348778a9ef41e493fd49
shakil <ataharnurshakil@gmail.com>
2023-09-17 17:17:09 +0600

    Assignment5

---
+++ index.js
@@ -0,0 +1,34 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const otpNumber = document.getElementById("otpNumber");
+const loginBtn = document.getElementById("loginBtn");
+const otpBtn = document.getElementById("otpBtn");
+const waring = document.getElementById("waring");
+const otp = document.getElementById("otp");
+
+const justInfo = {
+  eml: "shakil@.com",
+  pass: "0000",
+  otpNum : 0,  
+};
+loginBtn.onclick = () => {
+  const eml = email.value;
+  const pass = password.value;
+  const otp = Number(otpNumber.value);
+
+  if (eml !== "" && pass !== "" && otp !== "") {
+    if (eml === justInfo.eml && pass === justInfo.pass && otp === otpNum) {
+      window.location = "logout.html";
+    } else {
+      waring.innerText = "Your Informetion is not Correct ...";
+      waring.style.color = "green";
+    }
+  } else {
+    waring.innerText = "Please fillup Your Informetion ... ";
+    waring.style.color = "red";
+  }
+};
+otpBtn.onclick = () => {
+  otpNum = Math.floor(Math.random() * 10000);
+  otp.innerText = otpNum;
+};
---
+++ logout.js
@@ -0,0 +1,42 @@
+const work = document.getElementById("work");
+const time = document.getElementById("time");
+const workHours = document.getElementById("workHours");
+const addBtn = document.getElementById("addBtn");
+const waring = document.getElementById("waring");
+let students = [];
+
+addBtn.onclick = () => {
+  const value1 = work.value;
+  const value2 = time.value;
+  const value3 = workHours.value;
+  if(value1!=="" && value2!=="" &&value3!==""){
+    const object = {
+      newWork: value1,
+      newTime: value2,
+      newHours: value3,
+    };
+    students.push(object);
+    work.value="";
+    time.value="";
+    workHours.value="";
+    waring.innerText ="";
+  }else{
+    waring.innerText = "Please fillup Your Informetion ... ";
+    waring.style.color = "red";
+  }
+  
+  list();
+};
+
+const list = () => {
+  const sheet = document.getElementById("sheet");
+  sheet.innerHTML = "";
+  for (const student of students) {
+    const li = document.createElement("li");
+    li.innerText=`${student.newWork} = ${student.newTime} = ${student.newHours}`
+    sheet.appendChild(li);
+  }
+};
+
+
+
